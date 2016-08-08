d8fe08d4e08075e51a8d155281d9ffc385e26687
Gyj1109 - Linux <gyj1109@126.com>
2016-08-08 17:41:50 +0800

    增加安装脚本

---
+++ install.zsh
@@ -0,0 +1,7 @@
+mkdir ~/.kde4/share/apps/konsole
+mkdir $ZSH/custom/themes
+ln -s $PWD/gitconfig ~/.gitconfig
+ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/zshrc ~/.zshrc
+ln -s $PWD/agnoster.zsh-theme $ZSH/custom/themes/agnoster.zsh-theme
+ln -s $PWD/Solarized.colorscheme ~/.kde4/share/apps/konsole/Solarized.colorscheme
