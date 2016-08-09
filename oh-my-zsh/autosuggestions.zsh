45ff7a169c1b04d2fc1f6c9ae00c709779a92cb9
Gyj1109 - Linux <gyj1109@126.com>
2016-08-09 12:09:43 +0800

    为autosuggestions.zsh提供配置帮助

--- oh-my-zsh/autosuggestions.zsh
+++ oh-my-zsh/autosuggestions.zsh
@@ -1,2 +1,13 @@
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=4'
+# 0 : 淡灰 （基本不可见）
+# 1 : 红   （大红，错误命令提示色）
+# 2 : 绿豆色（正确命令提示色，powerline Git工作目录干净的背景色）
+# 3 : 橙   （powerline Git有更改，工作目录不干净的背景色）
+# 4 : 蓝   （powerline目录位置背景色）
+# 5 : 梅红
+# 6 : 青
+# 7 : 白
+# 其余数字均无效，显示为终端字体色（灰色）
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=6'
+
+# 按Ctrl+Space使用自动补全项，或者使用默认的Ctrl+E
 bindkey '^ ' autosuggest-accept
