4317874b8233be474c7434d6645c8efc8508b9da
Gyj1109 - Linux <gyj1109@126.com>
2016-08-09 14:10:28 +0800

    更换主题

--- install.zsh
+++ install.zsh
@@ -1,8 +1,10 @@
 mkdir $ZSH_CUSTOM/themes
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/bhilburn/powerlevel9k.git $ZSH_CUSTOM/themes/powerlevel9k
 ln -s $PWD/gitconfig ~/.gitconfig
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/zshrc ~/.zshrc
 ln -s $PWD/oh-my-zsh/agnoster-pyenv.zsh-theme $ZSH_CUSTOM/themes/agnoster-pyenv.zsh-theme
 ln -s $PWD/oh-my-zsh/autosuggestions.zsh $ZSH_CUSTOM/autosuggestions.zsh
+
