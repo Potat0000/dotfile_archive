e2c877b58d33be00472b6e1dc461338f51118c41
Gyj1109 - Linux <gyj1109@126.com>
2016-08-09 11:57:09 +0800

    整理.dotfile目录，删除不必要的文件，同时修改install脚本

--- install.zsh
+++ install.zsh
@@ -1,7 +1,8 @@
 mkdir $ZSH_CUSTOM/themes
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 ln -s $PWD/gitconfig ~/.gitconfig
 ln -s $PWD/vimrc ~/.vimrc
 ln -s $PWD/zshrc ~/.zshrc
-ln -s $PWD/agnoster-pyenv.zsh-theme $ZSH_CUSTOM/themes/agnoster-pyenv.zsh-theme
-ln -s $PWD/autosuggestions.zsh $ZSH_CUSTOM/autosuggestions.zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+ln -s $PWD/oh-my-zsh/agnoster-pyenv.zsh-theme $ZSH_CUSTOM/themes/agnoster-pyenv.zsh-theme
+ln -s $PWD/oh-my-zsh/autosuggestions.zsh $ZSH_CUSTOM/autosuggestions.zsh
